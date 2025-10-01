7865770b9c1315ceb4abbcdf707b3e96201afcc3
Ajdin Husetovic <ahusetovic1@gmail.com>
2025-10-01 17:37:39 +0200

    adjust max-w

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Typewriter = dynamic(() => import("typewriter-effect"), { ssr: false });
 const Hero = () => {
   return (
     <div className="relative h-screen w-full flex flex-col justify-center items-center px-6 text-white">
-      <div className="text-7xl lg:text-9xl text-center flex flex-col gap-8 z-10">
+      <div className="text-5xl lg:text-9xl text-center flex flex-col gap-4 lg:gap-8 z-10">
         <Typewriter
           options={{
             strings: ["MidlineCode"],
@@ -17,12 +17,12 @@ const Hero = () => {
             loop: true,
           }}
         />
-        <p className="text-2xl lg:text-3xl text-center">
+        <p className="text-xl lg:text-3xl text-center">
           Defining how your business is seen online.
         </p>
       </div>
 
-      <div className="absolute bottom-10 left-0 right-0 flex flex-col items-center justify-center z-10">
+      <div className="absolute bottom-20 left-0 right-0 flex flex-col items-center justify-center z-10">
         <span className="text-sm text-[#B0B0B0]">SCROLL</span>
         <ArrowDown size={48} color="#B0B0B0" />
       </div>
