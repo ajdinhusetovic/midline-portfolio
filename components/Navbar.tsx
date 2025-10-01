5effa2d4e4ac09846527079e3024bfc78685c2ff
Ajdin Husetovic <ahusetovic1@gmail.com>
2025-10-01 17:33:32 +0200

    adjust max-w

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,9 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="flex-shrink-0 text-white font-bold text-4xl"></div>
+          <div className="flex-shrink-0 text-white font-bold text-4xl hidden lg:block">
+            MidlineCode
+          </div>
 
           {/* Center Nav - Desktop */}
           <div
