9328231c5f56309070e6111bc9b672421000bfd7
Ajdin Husetovic <ahusetovic1@gmail.com>
2025-10-03 16:42:19 +0200

    update

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
   return (
     <section
       id="about-us"
-      className="min-h-screen max-w-7xl mt-10 md:mt-40 xl:mt-[-20px] mx-auto text-white flex flex-col items-center justify-center lg:gap-30"
+      className="min-h-screen max-w-7xl mt-25 md:mt-40 xl:mt-[-20px] mx-auto text-white flex flex-col items-center justify-center lg:gap-30"
     >
       <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold text-center">
         About Us
