5c86559c2021f627009c3d539012b2e28c473063
Ajdin Husetovic <ahusetovic1@gmail.com>
2025-12-04 17:54:35 +0100

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="text-slate-600 py-10 px-6 bg-blue-400 mb-5 mt-20 max-w-5xl mx-auto rounded-2xl">
+    <footer className="text-slate-600 py-10 px-6 bg-[#122e53] mb-5 mt-20 max-w-5xl mx-auto rounded-2xl">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6 text-center md:text-left">
         {/* Left Side */}
         <div>
-          <h2 className="text-slate-900 text-xl font-semibold mb-2">
-            MidlineCode
-          </h2>
-          <p className="text-slate-600">
+          <h2 className="text-white text-xl font-semibold mb-2">MidlineCode</h2>
+          <p className="text-gray-200">
             Building fast, modern websites that make businesses stand out.
           </p>
         </div>
@@ -18,7 +16,7 @@ export default function Footer() {
             📧{" "}
             <a
               href="mailto:contact@midlinecode.com"
-              className="text-slate-900 hover:text-white transition"
+              className="text-white hover:text-blue-400 transition"
             >
               contact@midlinecode.com
             </a>
@@ -27,7 +25,7 @@ export default function Footer() {
             📞{" "}
             <a
               href="tel:+17325341704"
-              className="text-slate-900 hover:text-white transition"
+              className="text-white hover:text-blue-400 transition"
             >
               +17325341704
             </a>
@@ -36,7 +34,7 @@ export default function Footer() {
       </div>
 
       {/* Divider */}
-      <div className="border-t border-gray-800 mt-8 pt-6 text-center text-sm text-gray-600">
+      <div className="border-t border-gray-800 mt-8 pt-6 text-center text-sm text-gray-400">
         © {new Date().getFullYear()} MidlineCode. All rights reserved.
       </div>
     </footer>
